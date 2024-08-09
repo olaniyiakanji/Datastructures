c853ceab7e843dae2456d2ad5271aac0574e87cd
umar oladimeji salami <96678883+olaniyiakanji@users.noreply.github.com>
2024-08-09 23:21:46 +0300

    Update SortColors.py

--- SortColors.py
+++ SortColors.py
@@ -28,7 +28,34 @@ Output: [0,1]
 def sort_colors(nums):
     low, mid, high = 0, 0, len(nums) - 1
     
-    while
-"""
-will finish it later 
-"""
+    while mid <= high:
+        if nums[mid] == 0:
+            nums[low], nums[mid] = nums[mid], nums[low]
+            low += 1
+            mid += 1
+        elif nums[mid] == 1:
+            mid += 1
+        else:
+            nums[high], nums[mid] = nums[mid], nums[high]
+            high -= 1
+
+# Test cases
+nums1 = [2, 0, 2, 1, 1, 0]
+sort_colors(nums1)
+print(nums1)  # Output: [0, 0, 1, 1, 2, 2]
+
+nums2 = [2, 0, 1]
+sort_colors(nums2)
+print(nums2)  # Output: [0, 1, 2]
+
+nums3 = [0]
+sort_colors(nums3)
+print(nums3)  # Output: [0]
+
+nums4 = [1, 0]
+sort_colors(nums4)
+print(nums4)  # Output: [0, 1]
+
+nums5 = [1, 2, 0]
+sort_colors(nums5)
+print(nums5)  # Output: [0, 1, 2]
