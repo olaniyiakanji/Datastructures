ed8eb28de0c4e6bcaa12e7af01ed81350b3e4bd9
umar oladimeji salami <96678883+olaniyiakanji@users.noreply.github.com>
2024-09-09 18:39:47 +0300

    Create RiverpodPostReqLearn.dart

---
+++ RiverpodPostReqLearn.dart
@@ -0,0 +1,54 @@
+class Example extends ConsumerStatefulWidget {
+  const Example({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ExampleState();
+}
+
+class _ExampleState extends ConsumerState<Example> {
+  // The pending addTodo operation. Or null if none is pending.
+  Future<void>? _pendingAddTodo;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // We listen to the pending operation, to update the UI accordingly.
+      future: _pendingAddTodo,
+      builder: (context, snapshot) {
+        // Compute whether there is an error state or not.
+        // The connectionState check is here to handle when the operation is retried.
+        final isErrored = snapshot.hasError && snapshot.connectionState != ConnectionState.waiting;
+
+        return Row(
+          children: [
+            ElevatedButton(
+              style: ButtonStyle(
+                // If there is an error, we show the button in red
+                backgroundColor: WidgetStatePropertyAll(
+                  isErrored ? Colors.red : null,
+                ),
+              ),
+              onPressed: () {
+                // We keep the future returned by addTodo in a variable
+                final future = ref
+                    .read(todoListProvider.notifier)
+                    .addTodo(Todo(description: 'This is a new todo'));
+
+                // We store that future in the local state
+                setState(() {
+                  _pendingAddTodo = future;
+                });
+              },
+              child: const Text('Add todo'),
+            ),
+            // The operation is pending, let's show a progress indicator
+            if (snapshot.connectionState == ConnectionState.waiting) ...[
+              const SizedBox(width: 8),
+              const CircularProgressIndicator(),
+            ]
+          ],
+        );
+      },
+    );
+  }
+}
