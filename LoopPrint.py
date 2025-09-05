2e414fd4c8fb1eed219bc71671a99f62be2fe523
umar oladimeji salami <96678883+olaniyiakanji@users.noreply.github.com>
2025-09-05 15:22:03 +0300

    Create LoopPrint.py

---
+++ LoopPrint.py
@@ -0,0 +1,10 @@
+def upade(val):
+  print("Here is the list of odd and even numbers within this range")
+  for i in range(val):
+    if i % 2 ==0:
+      print ( i + " is an even number common, ")
+    else:
+      print (i + " is an odd number sadly")
+  print("that all update available for now sorry")
+
+  
